480ce11c829d5d41d0b14f0d7e3ba44391349118
Jovanna Penn <jovannapenny@gmail.com>
2019-10-16 23:37:36 +1300

    design

--- Static/js.js
+++ Static/js.js
@@ -6,52 +6,52 @@ new Chart(document.getElementById("line-chart"), {
         data: [5.2,11.9,9.9,9.2,6,8,12.5,13.6,14,7,9.5,9.4],
         label: "Action",
         borderColor: "#3e95cd",
-        fill: false
+        fill: true
       }, {
         data: [5.5,1.6,1.9,2.9,3.4,4.6,5.9,6.3,9,8.5,11.6],
         label: "Thriller",
         borderColor: "#8e5ea2",
-        fill: false
+        fill: true
       }, {
         data: [3.6,1.9,2.7,7.5,4.7,4.8,2.5,2.3,1.6,1.3,8.1],
         label: "War",
         borderColor: "#3cba9f",
-        fill: false
+        fill: true
       }, {
         data: [1.4,0.8,0.8,0.4,2.1,2.3,2.1,2.7,3,2,3],
         label: "Sci-Fi",
         borderColor: "#e8c3b9",
-        fill: false
+        fill: true
       }, {
         data: [7.5,18.8,22.5,15.4,14.2,12.2,8.7,8.5,11.3,12.1,8.1],
         label: "Romance",
         borderColor: "#c45850",
-        fill: false
+        fill: true
       }, {
         data: [5,5.6,13,12,12,10.3,9.9,8.3,9.3,7.6,6.1],
         label: "Crime",
         borderColor: "#9d009e",
-        fill: false
+        fill: true
       }, {
         data: [0.7,1.1,9.3,7.2,6.7,5.2,2.7,2.1,1.1,1.1,0.9],
         label: "Musical",
         borderColor: "#9d009e",
-        fill: false
+        fill: true
       }, {
         data: [9.8,18.4,32.1,30.7,27.6,27.3,25.1,24.5,25.6,25.7,23.5],
         label: "Comedy",
         borderColor: "#019e00",
-        fill: false
+        fill: true
       }, {
         data: [2.5,2.1,1.5,1.9,2.1,3.7,5.7,6.4,5,7,10.3],
         label: "Horror",
         borderColor: "#ff9600",
-        fill: false
+        fill: true
       }, {
         data: [3.1,2.2,1.2,1.9,1.8,3.3,4.8,4.9,8.9,18.8,23.6],
         label: "Documentary",
         borderColor: "#fff700",
-        fill: false
+        fill: true
       }
     ]
   },
--- run.py
+++ run.py
@@ -9,12 +9,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/action', methods=['GET'])
-def action():
-    con = sqlite3.connect (GENRESDB)
-    cur = con.execute('SELECT * FROM action')
-
-    for row in cur:
-        action.append(list(row))
-    con.close()
-    return jsonify(action)
+@app.route('/genres', methods=['GET'])
+def populargenres():
+    return jsonify ('genres')
